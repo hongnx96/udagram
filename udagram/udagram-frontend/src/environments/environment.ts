ce0bb8c1fec1fd04d84ed2c0df4b34da327f5a0a
FPT_Nguyen Xuan Hong/ホン <hongnx96@gmail.com>
2023-11-12 15:36:50 +0700

    update env fe

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api1-env.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api.us-east-1.elasticbeanstalk.com/api/v0',
   // apiHost: 'http://localhost:8080/api/v0',
 };
 
